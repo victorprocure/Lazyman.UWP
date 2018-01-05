18cc26147c5a461f083d5b0cdea7ac833911b731
Victor Procure <victor@victorprocure.co>
2018-01-05 14:50:48 -0700

    Initial

---
+++ UI/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="Procure Software Development">
+//   Copyright (c) Procure Software Development
+// </copyright>
+// <summary>
+//   The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UI
+{
+    using Windows.UI.Xaml.Controls;
+
+    /// <inheritdoc cref="Page" />
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <inheritdoc />
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
